67f902c366f124e6bd1831ef5423b3b2f59b4a1a
mjc284 <mjc284@cam.ac.uk>
2022-11-20 16:42:12 +0000

    Created an assembler and sample assembly program.

--- src/software/assembler.cpp
+++ src/software/assembler.cpp
@@ -4,6 +4,16 @@
 
 using namespace std;
 
+bool is_number(string str)
+{
+    return (str.find_first_not_of("0123456789") == string::npos);
+}
+
+bool is_scalphabet(string str)
+{
+    return (str.find_first_not_of("abcdefghijklmnopqrstuvwxyz") == string::npos);
+}
+
 int main(int argc, char** argv)
 {
     string ifile_path = argv[1];
@@ -37,14 +47,145 @@ int main(int argc, char** argv)
     ofile << "CONTENT BEGIN\n";
 
     string line;
+    string instruction;
+    string data;
+    string format;
+    int range = 0;
+    int address;
+    int processed_data;
+    int index = 0;
     if(ifile)
     {
+        address = 0;
         while(getline(ifile, line))
         {
-            ofile << "\t";
-            
-            ofile << "\n";
+            if(address > 511)
+            {
+                cerr << "Assembly code exceeds 512 instructions!";
+                exit(0);
+            }
+            range = 0;
+            for(int i = 0; i < line.length(); i++)
+            {
+                if(line.at(i) == '#')
+                {
+                    break;
+                }
+                if((i > 4) and (line.at(i) == ' '))
+                {
+                    break;
+                }
+                range = i;
+            }
+            if(range != 0)
+            {
+                instruction = line.substr(0, 3);
+                data = line.substr(4, range-3);
+
+                try
+                {
+                    if(is_number(data))
+                    {
+                        processed_data = stoul(data);
+                    }
+                    else if(is_number(data.substr(2)))
+                    {
+                        format = data.substr(0, 2);
+                        data = data.substr(2);
+                        if(format == "0d")
+                        {
+                            processed_data = stoul(data);
+                        }
+                        else if(format == "0b")
+                        {
+                            processed_data = stoul(data, nullptr, 2);
+                        }
+                        else if(format == "0x")
+                        {
+                            processed_data = stoul(data, nullptr, 16);
+                        }
+                        else
+                        {
+                            throw runtime_error("Invalid data at line ");
+                        }
+                    }
+                    else
+                    {
+                        throw runtime_error("Invalid data at line ");
+                    }
+
+                    if(instruction == "out")
+                    {
+                        if(processed_data > 0b111111111111111)
+                        {
+                            throw runtime_error("Output data is over the 15 bit limit at line ");
+                        }
+                        ofile << "\t" << index << "\t:\t" << processed_data << ";\n";
+                        index++;
+                    }
+                    else if(instruction == "jmp")
+                    {
+                        if(processed_data > 0b111111111)
+                        {
+                            throw runtime_error("Address data is over the 9 bit limit at line ");
+                        }
+                        ofile << "\t" << index << "\t:\t" << ((0b11*0b1000000000)+processed_data)*0b100000 << ";\n";
+                        index++;
+                    }
+                    else if(instruction == "beq")
+                    {
+                        if(processed_data > 0b111111111111)
+                        {
+                            throw runtime_error("Operand data is over the 12 bit limit at line");
+                        }
+                        ofile << "\t" << index << "\t:\t" << (0b1000*0b1000000000000)+processed_data << ";\n";
+                        index++;
+                    }
+                    else if(instruction == "bne")
+                    {
+                        if(processed_data > 0b111111111111)
+                        {
+                            throw runtime_error("Operand data is over the 12 bit limit at line ");
+                        }
+                        ofile << "\t" << index << "\t:\t" << (0b1001*0b1000000000000)+processed_data << ";\n";
+                        index++;
+                        
+                    }
+                    else if(instruction == "blt")
+                    {
+                        if(processed_data > 0b111111111111)
+                        {
+                            throw runtime_error("Operand data is over the 12 bit limit at line ");
+                        }
+                        ofile << "\t" << index << "\t:\t" << (0b1010*0b1000000000000)+processed_data << ";\n";
+                        index++;
+                        
+                    }
+                    else if(instruction == "bge")
+                    {
+                        if(processed_data > 0b111111111111)
+                        {
+                            throw runtime_error("Operand data is over the 12 bit limit at line ");
+                        }
+                        ofile << "\t" << index << "\t:\t" << (0b1011*0b1000000000000)+processed_data << ";\n";
+                        index++;
+                    }
+                    else
+                    {
+                        throw runtime_error("Invalid instruction at line ");
+                    }
+                }
+                catch(const std::exception& e)
+                {
+                    std::cerr << e.what() << address << '\n';
+                }
+            }
+            address++;
         }
     }
 
+    ofile << "\t" << "[" << index << "..511]" << "\t:\t0;\n";
+    ofile << "END;";
+    ofile.close();
+    ifile.close();
 }
